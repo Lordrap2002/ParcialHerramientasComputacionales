4a53b1080f5945508d653008dedf5b0145df67a8
Belhill100 <75393649+Belhill100@users.noreply.github.com>
2020-12-02 15:34:59 -0500

    Add files via upload

---
+++ cafeteria.py
@@ -0,0 +1,15 @@
+def cafeteria():
+    n=input("Introduzca su cedula:")
+    r=input("Rol:")
+    cod=int(input("Codigo del Producto:"))
+    u=int(input("Cuantos va a llevar:"))
+    v=int(input("Precio del Proudcto:"))
+    precio=v*u
+    if r=="profesor":
+        total=precio*0.8
+    elif r=="estudiante":
+        total=precio*0.5
+
+    print("El %s con cedula %s, debe pagar %d por el producto %d" % (r,n,total,cod))
+        
+cafeteria()    
